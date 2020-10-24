6e25205e076f9b43c50913bd94de3adefaf332fe
JasonLcs <jasonlcs@gmail.com>
2020-10-24 09:30:04 +0800

    第一版釋出

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
 input.onGesture(Gesture.LogoUp, function () {
     movePositionByShake("DOWN")
 })
+function displayBoard () {
+    setPointLight(1, 1, list[0])
+    setPointLight(2, 1, list[1])
+    setPointLight(3, 1, list[2])
+    setPointLight(1, 2, list[3])
+    setPointLight(2, 2, list[4])
+    setPointLight(3, 2, list[5])
+    setPointLight(1, 3, list[6])
+    setPointLight(2, 3, list[7])
+    setPointLight(3, 3, list[8])
+}
 input.onGesture(Gesture.TiltLeft, function () {
     movePositionByShake("LEFT")
 })
@@ -45,7 +56,6 @@ function movePositionByShake (direction: string) {
     showIndex(idxX, idxY)
 }
 function setChess (player: string, x: number, y: number) {
-    let list: string[] = []
     if (x == 1 && y == 1) {
         list[0] = player
     } else if (x == 2 && y == 1) {
@@ -69,28 +79,39 @@ function setChess (player: string, x: number, y: number) {
     }
 }
 input.onButtonPressed(Button.A, function () {
-    setChess("X", idxX, idxY)
+    setChess("O", idxX, idxY)
+    led.plot(idxX, idxY)
 })
 input.onGesture(Gesture.LogoDown, function () {
     movePositionByShake("UP")
 })
 input.onButtonPressed(Button.B, function () {
-    setChess("O", idxX, idxY)
+    setChess("X", idxX, idxY)
+    led.toggle(idxX, idxY)
 })
 input.onGesture(Gesture.TiltRight, function () {
     movePositionByShake("RIGHT")
 })
+function setPointLight (x: number, y: number, player: string) {
+    if (player == "O") {
+        led.plot(x, y)
+    } else if (player == "X") {
+        led.toggle(x, y)
+    } else {
+        led.unplot(x, y)
+    }
+}
 function showIndex (x: number, y: number) {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    for (let index = 0; index <= 2; index++) {
+        led.unplot(index + 1, 0)
+    }
+    for (let index = 0; index <= 2; index++) {
+        led.unplot(0, index + 1)
+    }
     led.plot(x, 0)
     led.plot(0, y)
 }
+let list: string[] = []
 let idxY = 0
 let idxX = 0
 basic.clearScreen()
@@ -99,5 +120,5 @@ idxY = 1
 let chessState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 showIndex(1, 1)
 basic.forever(function () {
-	
+    displayBoard()
 })
